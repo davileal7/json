b041f82c74a805e40a6334497860c22e7b6fbf67
davileal7 <108898208+davileal7@users.noreply.github.com>
2023-02-03 18:06:15 -0300

    json conversões

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+const objs = [
+    {
+        nome: 'Davi',
+        idade: 36,
+        esta_trabalhando: true,
+        work_details: {
+            profisssao: 'Programador',
+            empresa: 'Empresa X'
+        },
+        hobbies: ['Academia', 'Games']
+    },
+    {
+        nome : 'Rafael',
+        idade: 26,
+        esta_trabalhando: false,
+        work_details: {
+            profisssao : null,
+            empresa: null 
+        },
+        hobbies: ['Corrida','Futebol']
+    },
+]
+
+// JSON
+// converter objeto para json
+const jsonData = JSON.stringify(objs)
+
+console.log(jsonData)
+console.log(typeof jsonData)
+
+
+// converter json para objeto
+const objData = JSON.parse(jsonData);
+
+console.log(objData)
+console.log(typeof objData)
+
+objData.map((pessoa) => {
+    console.log(pessoa.nome)
+})
